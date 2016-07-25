ee14b3e487d2132f2c32347278ce03aaf71a59a2
Ash Kumar <kumar.ashwin@outlook.com>
2016-07-25 16:46:39 -0400

    Finished section on Email confirmation

---
+++ LearningEmailAuth/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace LearningEmailAuth
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
