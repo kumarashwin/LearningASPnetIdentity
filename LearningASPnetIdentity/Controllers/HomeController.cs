26437898dd2900f9d606e60413708c9f6d22e7c9
Ash Kumar <kumar.ashwin@outlook.com>
2016-07-25 13:00:33 -0400

    Basic setup/scaffolding with first user created in controller code

---
+++ LearningASPnetIdentity/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace LearningASPnetIdentity.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public async Task<ActionResult> Index()
+        {
+            var context = new IdentityDbContext<IdentityUser>(); //DefaultConnection
+            var store = new UserStore<IdentityUser>(context);
+            var manager = new UserManager<IdentityUser>(store);
+
+            var email = "foo@bar.com";
+            var password = "Passw0rd";
+            var user = await manager.FindByEmailAsync(email);
+
+            if (user == null)
+            {
+                user = new IdentityUser {
+                    UserName = email,
+                    Email = email };
+
+                await manager.CreateAsync(user, password);
+            }
+
+
+
+            return Content("Hello, Index");
+        }
+    }
+}
---
+++ LearningASPnetIdentity/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LearningASPnetIdentity.MvcApplication" Language="C#" %>
